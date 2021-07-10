9da96f3f153fe394731bbca9102eefc03290efe9
Bauerka <bauerka@Bauerkas-MacBook-Pro.local>
2021-07-10 10:08:11 +0200

    add guess-number.py with plan of the game

---
+++ guess-number.py
@@ -0,0 +1,12 @@
+from random import randint
+the_number = randint(1,10)
+print(the_number)
+# Game Plan
+# Import random and set a variable with randint from 1-100
+# Get input from user and check if it's a number (if stat. with type or error control)
+# Check if number is small and print "To small!" and return to first step
+# Check if number is high and print "To big!" and return to first step
+# If number given by user is equal to computer's numer pring 'You win!' and end the game.
+
+
+
