72a2a2e124b5f3ad0b190446b680f7cab1b774c0
Bauerka <bauerka@Bauerkas-MacBook-Pro.local>
2021-07-11 18:39:29 +0200

    Add guess-number-optimise.py Updated guess-number.py

--- guess-number-optimise.py
+++ guess-number-optimise.py
@@ -5,7 +5,6 @@ the_number = randint(1, 100)
 
 # Print for showing the number > debugging
 print(f'The number is: {the_number}')
-user_guess = 0
 
 # Print introduction
 intro_string = '''
@@ -21,31 +20,25 @@ Good luck!
 
 print(intro_string)
 
-# Get input from user and check if it's a number (if stat. with type or error control)
-try:
-    user_guess = int(input('Your guess: '))
-except ValueError:
-    print('Only numbers allowed')
-    user_guess = int(input('Your guess: '))
 
-# def get_user_guess():
-#     try:
-#         user_guess = int(input('Your guess: '))
-#     except ValueError:
-#         print('Only numbers allowed')
-#         user_guess = int(input('Your guess: '))
+def get_user_guess():
+    try:
+        usr_input = int(input('Your guess: '))
+    except ValueError:
+        print('Only numbers allowed')
+        usr_input = int(input('Your guess: '))
+    return usr_input
 
-# get_user_guess()
+
+user_guess = get_user_guess()
 
 while True:
     if the_number > user_guess:  # Check if number is small and print "To small!" and return to first step
         print('To small!')
-        # get_user_guess()
-        user_guess = int(input('The number is in a range between 1 and 100. \n Your guess: '))
+        user_guess = get_user_guess()
     elif the_number < user_guess:  # Check if number is high and print "To big!" and return to first step
         print('To big!')
-        # get_user_guess()
-        user_guess = int(input('The number is in a range between 1 and 100. \n Your guess: '))
+        user_guess = get_user_guess()
     else:  # If number given by user is equal to computer's number print 'You win!' and end the game.
         print('You win!')
         break
